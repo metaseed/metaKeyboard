a485db3407b02fade16f875399be48da925c302b
metaseed <metaseed@gmail.com>
2017-07-19 18:46:58 +0800

    register

--- metaKeyboard/register.ps1
+++ metaKeyboard/register.ps1
@@ -1,3 +1,38 @@
+function Parse-IniFile ($file) {
+    $ini = @{}
+    switch -regex -file $file {
+        #Section.
+        "^\[(.+)\]$" {
+            $section = $matches[1].Trim()
+            $ini[$section] = @{}
+            continue
+        }
+        #Int.
+        "^\s*([^#].+?)\s*=\s*(\d+)\s*$" {
+            $name, $value = $matches[1..2]
+            $ini[$section][$name] = [int]$value
+            continue
+        }
+        #Decimal.
+        "^\s*([^#].+?)\s*=\s*(\d+\.\d+)\s*$" {
+            $name, $value = $matches[1..2]
+            $ini[$section][$name] = [decimal]$value
+            continue
+        }
+        #Everything else.
+        "^\s*([^#].+?)\s*=\s*(.*)" {
+            $name, $value = $matches[1..2]
+            $ini[$section][$name] = $value.Trim()
+        }
+    }
+    $ini
+}
+
+
+$ini = Parse-IniFile($PSScriptRoot + "\config.ini")
+
 [Environment]::SetEnvironmentVariable("g", $env:USERPROFILE + "\Documents\GitHub", [System.EnvironmentVariableTarget]::User)
 [Environment]::SetEnvironmentVariable("r", $env:USERPROFILE + "\Source\Repos", [System.EnvironmentVariableTarget]::User)
-[Environment]::SetEnvironmentVariable("w", $env:USERPROFILE + "M:\Workspace", [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("w", $ini["register"]["workDirectory"], [System.EnvironmentVariableTarget]::User)
+
+
